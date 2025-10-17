5affb2770e4eea23b0efcb4a1dc8d2e210e39031
AbdEl-WahabGhareeb <abdosileem48@gmail.com>
2025-10-17 18:47:58 +0300

    Deploying

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,29 +1,30 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+    const date = new Date();
     return [
         {
-            url: 'https://taamco.com',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
+            url: "https://taamco.com",
+            lastModified: date,
+            changeFrequency: "monthly",
             priority: 1,
         },
         {
-            url: 'https://taamco.com/about',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
+            url: "https://taamco.com/about",
+            lastModified: date,
+            changeFrequency: "monthly",
             priority: 0.8,
         },
         {
-            url: 'https://taamco.com/services',
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
+            url: "https://taamco.com/services",
+            lastModified: date,
+            changeFrequency: "weekly",
             priority: 0.9,
         },
         {
-            url: 'https://taamco.com/projects',
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
+            url: "https://taamco.com/projects",
+            lastModified: date,
+            changeFrequency: "weekly",
             priority: 0.9,
         },
     ];
